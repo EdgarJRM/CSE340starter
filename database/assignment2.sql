11a98666ea96e57b05e776361629405a7d7b3a9e
EdgarJRM <edgarrodriguezm@hotmail.com>
2023-06-17 21:27:05 -0500

    Week 2

---
+++ database/assignment2.sql
@@ -0,0 +1,4 @@
+SELECT * FROM classification;
+SELECT * FROM classification WHERE classification_id = 2;
+UPDATE classification SET classification_name = 'Minivan' WHERE classification_id = 5;
+SELECT * FROM classification;
